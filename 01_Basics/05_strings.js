c28f4d9b4ef073392c7c21befe3a0d4e91021d5e
erprpatil <121144148+erprpatil@users.noreply.github.com>
2024-03-05 12:56:28 +0000

    srtrings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,19 @@
+   const name = "Prashant"
+   const repoCount = 100
+
+   console.log(name + repoCount + " Value");
+
+   console.log(`My name is ${name} and my repo count is ${repoCount}`); //new method called string interpolation
+
+   const gameName = new String ('prashant-Patil') // defining string through object
+
+   // console.log(gameName[2]);
+   // console.log(gameName.__proto__);
+
+   // console.log(gameName.length);
+   // console.log(gameName.toUpperCase());
+   // console.log(gameName);
+
+   console.log(gameName.charAt(2));
+   console.log(gameName.indexOf('a'));
+   console.log(gameName);
