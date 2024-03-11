e65d2c5b9fb74e57383541bcd9993f8bf268898f
erprpatil <121144148+erprpatil@users.noreply.github.com>
2024-03-11 12:09:20 +0000

    num&math

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(6));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString()); //100,000
+// console.log(hundreds.toLocaleString('en-IN')); //1,00,000
+
+
+
+// +++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round (4.5));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+const randomNum = Math.random();
+
+console.log(randomNum);
+
+console.log(randomNum*10);
+
+console.log((randomNum*10) + 1);
