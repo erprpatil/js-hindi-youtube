017b720955f64431d0db391a23c0a88847c21eaa
erprpatil <121144148+erprpatil@users.noreply.github.com>
2024-03-11 12:38:14 +0000

    dates

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,24 @@
+// Date
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23) // Month starts with 0 in JS
+
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+
+// let myNewCreatedDate = new Date (2023, 0, 23, 5, 3)
+// console.log(myNewCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date ("2023-01-14") //Can be changed to any date format
+console.log(myCreatedDate.toLocaleString());
