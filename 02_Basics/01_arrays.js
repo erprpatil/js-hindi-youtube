09aaf10e4cb1f8bdaf231030486eb05bed7e8562
erprpatil <121144148+erprpatil@users.noreply.github.com>
2024-03-12 12:51:23 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,13 @@
+// arrays
+
+const myArr = [0, 1 ,2, 3, 4, 5]
+const myHeroes = ["shaktimaan", "naagraj"]
+
+console.log(myHeroes[1]);
+const myArr2 = new Array (1,2,3, 4, 5)
+console.log(myArr[1]); //Value at index 0
+
+// Array methods
+
+myArr.push(6);
+console.log(myArr);
