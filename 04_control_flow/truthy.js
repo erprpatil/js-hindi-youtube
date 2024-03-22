a0022f2b16f669532d7ebd228e97cad26136dbbe
erprpatil <121144148+erprpatil@users.noreply.github.com>
2024-03-22 11:57:23 +0000

    truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -25,6 +25,24 @@ truthy values
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length) {
+if (Object.keys(emptyObj).length === 0) {
     console.log("object is empty");
 }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1
+val1 = 5 ?? 10
+val1 = null ?? 10
+var1 = undefined ?? 15
+
+
+console.log(val1);
+
+// Terniary Operator
+
+condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80"); 
